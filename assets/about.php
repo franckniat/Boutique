1acb329b0ab772d0bd38e24ec2a5e26f498ea0f6
franckniat <niatmanuel2002@gmail.com>
2023-04-05 22:59:42 +0100

    First commit

---
+++ assets/about.php
@@ -0,0 +1,65 @@
+<!-- offcanvas of about -->
+<div class="offcanvas offcanvas-start shadow-lg" data-bs-scroll="false" data-bs-backdrop="false" tabindex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
+        <div class="offcanvas-header">
+          <h5 class="offcanvas-title text-bold text-center" id="offcanvasWithBothOptionsLabel">Horaires d'ouverture et fermeture</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <hr class="my-0">
+        <div class="offcanvas-body">
+            <table class="table">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Jours</th>
+                    <th scope="col">Ouverture (h)</th>
+                    <th scope="col">Fermeture (h)</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>Lundi</td>
+                    <td>07h30</td>
+                    <td>22h00</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">2</th>
+                    <td>Mardi</td>
+                    <td>07h30</td>
+                    <td>22h00</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">3</th>
+                    <td>Mercredi</td>
+                    <td>07h30</td>
+                    <td>22h00</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">4</th>
+                    <td>Jeudi</td>
+                    <td>10h30</td>
+                    <td>22h00</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">5</th>
+                    <td>Vendredi</td>
+                    <td>07h30</td>
+                    <td>22h00</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">6</th>
+                    <td>Samedi</td>
+                    <td>08h00</td>
+                    <td>22h00</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">7</th>
+                    <td>Dimanche</td>
+                    <td>08h00</td>
+                    <td>22h30</td>
+                  </tr>
+                </tbody>
+            </table>
+            
+        </div>
+</div>
---
+++ assets/header.php
@@ -0,0 +1,9 @@
+<!-- Page header with logo and tagline-->
+<header class="py-5 bg-dark border-bottom mb-4">
+        <div class="container">
+            <div class="text-center text-white my-5">
+                <h1 class="fw-bolder">Bienvenue au Grand Carrefour !</h1>
+                <p class="lead mb-0">Vous faire plaisir nous fait plaisir. Entrez par curiosité et restez par satisfaction !</p>
+            </div>
+        </div>
+    </header>
